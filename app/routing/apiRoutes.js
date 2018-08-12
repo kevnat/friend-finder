6c1f7b687b659bd584f797080283bea78e8c0bee
Kevin Nathan <kevmaxnathan@gmail.com>
2018-08-12 10:44:45 -0400

    moved friendData into apiRouter

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,12 @@ module.exports = function(app) {
         res.json(friends)
     });
 
-    app.post("/api/friends", function(req, res){
-        console.log(req.body);
+    app.post("/api/friends", function(req, res) {
+        let scoresVals = req.body.scores.map(Number);
+        for (var i = 0; i < req.body.scores.length; i++) {
+            req.body.scores.splice(i, 1, scoresVals[i]);
+        }
+        console.log("Adding" + req.body + "\n-----------------------");
         friends.push(req.body);
         console.log(friends);
     })
